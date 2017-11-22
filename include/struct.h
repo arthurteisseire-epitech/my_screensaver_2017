af1dd61a70495556c4653ee326ade01d68be5308
Arthur <arthur.teisseire@epitech.eu>
2017-11-22 17:08:01 +0100

    [+]struct.h : obj and position struct added

---
+++ include/struct.h
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2017
+** File Name : struct.h
+** File description:
+** By Arthur Teisseire
+*/
+
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#include <SFML/Graphics.h>
+#include <stdlib.h>
+
+typedef struct position {
+	float x;
+	float y;
+} position_t;
+
+typedef struct obj {
+	float speed;
+	position_t pos;
+	sfSprite sprite;
+	sfTexture texture;
+} obj_t;
+
+#endif
