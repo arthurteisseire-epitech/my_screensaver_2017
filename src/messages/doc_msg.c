6df171da6d8e0ef957132ea42c955fa4b83b20c4
Arthur <arthur.teisseire@epitech.eu>
2017-12-06 23:14:14 +0100

    [*]doc_msg : coding style ok

--- src/messages/doc_msg.c
+++ src/messages/doc_msg.c
@@ -13,11 +13,13 @@ void put_help()
 	my_putstr("animation rendering in a CSFML window.\n");
 	my_putstr("\nUSAGE\n");
 	my_putstr(" ./my_screensaver [OPTIONS] animation_id\n");
-	my_putstr("  animation_id    ID of the animation to process (between 1 and ");
+	my_putstr("  animation_id    ");
+	my_putstr("ID of the animation to process (between 1 and ");
 	my_put_nbr(MAX_ID);
 	my_putstr(").\n");
 	my_putstr("\nOPTIONS\n");
-	my_putstr(" -d               print the description of all the animations and quit.\n");
+	my_putstr(" -d               ");
+	my_putstr("print the description of all the animations and quit.\n");
 	my_putstr(" -h               print the usage and quit.\n");
 	my_putstr("\nUSER INTERACTIONS\n");
 	my_putstr(" LEFT_ARROW       switch to the previous animation.\n");
