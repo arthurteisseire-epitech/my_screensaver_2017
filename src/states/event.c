6a67a617481eb59d5773f385ba11b4861446ac9a
Arthur <arthur.teisseire@epitech.eu>
2017-12-06 22:42:22 +0100

    Event with arrows work now

--- src/states/event.c
+++ src/states/event.c
@@ -7,6 +7,17 @@
 
 #include "screen.h"
 
+static void check_key(screen_t *sc, sfEvent *event)
+{
+	if (event->key.code == sfKeyLeft) {
+		sc->id_anim--;
+		if (sc->id_anim < 0)
+			sc->id_anim = MAX_ID - 1;
+	}
+	else if (event->key.code == sfKeyRight)
+		sc->id_anim = (sc->id_anim + 1) % MAX_ID;
+}
+
 void event(screen_t *sc)
 {
 	sfEvent event;
@@ -14,4 +25,7 @@ void event(screen_t *sc)
 	while (sfRenderWindow_pollEvent(sc->window, &event))
 		if (event.type == sfEvtClosed)
 			sfRenderWindow_close(sc->window);
+		else if (event.type == sfEvtKeyPressed)
+			check_key(sc, &event);
 }
+
