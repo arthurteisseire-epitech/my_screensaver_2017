75abd8342b6a770c893816f6604e6f46c6dae0fd
Arthur <arthur.teisseire@epitech.eu>
2017-12-06 23:38:00 +0100

    [+]Just a test done to see if my square move : it works

--- src/animations/anim1.c
+++ src/animations/anim1.c
@@ -9,8 +9,17 @@
 
 void anim1(screen_t *sc)
 {
-	my_put_square(sc->buffer, 0, 0, 200, 200, sfRed);
+	static int x = 0;
+	static int y = 0;
+
+	my_put_square(sc->buffer, x, y, 200, 200, sfRed);
 	sfTexture_updateFromPixels(sc->texture, sc->buffer->pixels, WIDTH, HEIGHT, 0, 0);
+	x++;
+	y++;
+	if (x + 200 > WIDTH)
+		x = 0;
+	if (y + 200 > HEIGHT)
+		y = 0;
 }
 
 void anim2(screen_t *sc)
