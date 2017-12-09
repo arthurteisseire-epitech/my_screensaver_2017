af8230e25322bcb592887f633c20b0282f5cb28d
Arthur <arthur.teisseire@epitech.eu>
2017-12-09 12:27:28 +0100

    if forest replaced by switch case

--- src/tools/map.c
+++ src/tools/map.c
@@ -12,18 +12,19 @@ sfColor map(clock_color_t *ct)
 	int sec = (int)ct->sec % 6;
 	float s_time = get_decimal(ct->sec);
 
-	if (sec == 0) {
-		ct->color.b = s_time * 255;
-	} else if (sec == 1) {
-		ct->color.r = 255 - s_time * 255;
-	} else if (sec == 2) {
-		ct->color.g = s_time * 255;
-	} else if (sec == 3) {
-		ct->color.b = 255 - s_time * 255;
-	} else if (sec == 4) {
-		ct->color.r = s_time * 255;
-	} else if (sec == 5) {
-		ct->color.g = 255 - s_time * 255;
+	switch (sec) {
+		case 0: ct->color.b = s_time * 255;
+			break;
+		case 1: ct->color.r = 255 - s_time * 255;
+			break;
+		case 2: ct->color.g = s_time * 255;
+			break;
+		case 3: ct->color.b = 255 - s_time * 255;
+			break;
+		case 4: ct->color.r = s_time * 255;
+			break;
+		case 5: ct->color.g = 255 - s_time * 255;
+			break;
 	}
 	return (ct->color);
 }
