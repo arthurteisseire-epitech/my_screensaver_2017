ea1654d15fde0d78eb8b9bcc7cd7c6f33689e4e8
Arthur <arthur.teisseire@epitech.eu>
2017-12-09 21:14:56 +0100

    doc message : coding style ok

--- include/messages.h
+++ include/messages.h
@@ -12,8 +12,8 @@
 #include <unistd.h>
 
 void check_args(char *str);
-void put_help();
-void put_description();
+void put_help(void);
+void put_description(void);
 void put_invalid_arg(char *str);
 void put_bad_nb(int ac);
 void put_bad_index(int id_index);
--- src/messages/doc_msg.c
+++ src/messages/doc_msg.c
@@ -8,30 +8,36 @@
 #include "screen.h"
 #include "my.h"
 
-void put_help()
+void put_help(void)
 {
-	my_putstr("animation rendering in a CSFML window.\n");
-	my_putstr("\nUSAGE\n");
-	my_putstr(" ./my_screensaver [OPTIONS] animation_id\n");
-	my_putstr("  animation_id    ");
-	my_putstr("ID of the animation to process (between 1 and ");
+	my_putstr(
+	"animation rendering in a CSFML window.\n"
+	"\nUSAGE\n"
+	" ./my_screensaver [OPTIONS] animation_id\n"
+	"  animation_id    "
+	"ID of the animation to process (between 1 and "
+	);
 	my_put_nbr(MAX_ID);
-	my_putstr(").\n");
-	my_putstr("\nOPTIONS\n");
-	my_putstr(" -d               ");
-	my_putstr("print the description of all the animations and quit.\n");
-	my_putstr(" -h               print the usage and quit.\n");
-	my_putstr("\nUSER INTERACTIONS\n");
-	my_putstr(" LEFT_ARROW       switch to the previous animation.\n");
-	my_putstr(" RIGHT_ARROW      switch to the next animation.\n\n");
+	my_putstr(
+	").\n"
+	"\nOPTIONS\n"
+	" -d               "
+	"print the description of all the animations and quit.\n"
+	" -h               print the usage and quit.\n"
+	"\nUSER INTERACTIONS\n"
+	" LEFT_ARROW       switch to the previous animation.\n"
+	" RIGHT_ARROW      switch to the next animation.\n\n"
+	);
 }
 
-void put_description()
+void put_description(void)
 {
-	my_putstr("1:\tNoise in black and white with squares\n");
-	my_putstr("2:\tSquares change position with time to give a 3d impress\n");
-	my_putstr("3:\t4 squares that comes faster and faster in x direction\n");
-	my_putstr("4:\tTeddy bear is in the place ! bigger and bigger, then the opposit\n");
-	my_putstr("5:\tTeddy become lower and lower then explode to be bigger\n");
-	my_putstr("6:\tSome funny colors for Teddy !\n");
+	my_putstr(
+	"1:\tNoise in black and white with squares\n"
+	"2:\tSquares change position with time to give a 3d impress\n"
+	"3:\t4 squares that comes faster and faster in x direction\n"
+	"4:\tTeddy bear is in the place ! bigger and bigger, then the opposit\n"
+	"5:\tTeddy become lower and lower then explode to be bigger\n"
+	"6:\tSome funny colors for Teddy !\n"
+	);
 }
